7447e124d6d33dfd2ae0865a7d305a41427b9e72
xBZZZZ <91378413+xBZZZZ@users.noreply.github.com>
2025-12-01 17:08:13 +0200

    docs(cli): update description of getCString
    question: how are bytes converted to javascript string?
    answer: UTF-8
    question: what if pointer points to D0 00 bytes (invalid UTF-8)?
    answer: `new TextDecoder().decode(Uint8Array.of(0xD0))` returns "\uFFFD", getCString also returns "\uFFFD"
    
    Signed-off-by: xBZZZZ <91378413+xBZZZZ@users.noreply.github.com>

--- cli/tsc/dts/lib.deno.ns.d.ts
+++ cli/tsc/dts/lib.deno.ns.d.ts
@@ -6033,11 +6033,40 @@ declare namespace Deno {
     getFloat64(offset?: number): number;
     /** Gets a pointer at the specified byte offset from the pointer */
     getPointer<T = unknown>(offset?: number): PointerValue<T>;
-    /** Gets a C string (`null` terminated string) at the specified byte offset
-     * from the pointer. */
+    /** Gets a UTF-8 encoded string at the specified byte offset until 0 byte.
+     *
+     * Returned string doesn't include U+0000 character.
+     *
+     * Invalid UTF-8 causes U+FFFD character(s) in returned string.
+     *
+     * Similar to:
+     * ```ts
+     * function getCString(this: Deno.UnsafePointerView, offset: number = 0) {
+     *   let byteLength = 0;
+     *   while (this.getUint8(offset + byteLength)) {
+     *     ++byteLength;
+     *   }
+     *   return new TextDecoder().decode(this.getArrayBuffer(byteLength, offset));
+     * }
+     * ``` */
     getCString(offset?: number): string;
-    /** Gets a C string (`null` terminated string) at the specified byte offset
-     * from the specified pointer. */
+    /** Gets a UTF-8 encoded string at the specified byte offset from the specified pointer until 0 byte.
+     *
+     * Returned string doesn't include U+0000 character.
+     *
+     * Invalid UTF-8 causes U+FFFD character(s) in returned string.
+     *
+     * Similar to:
+     * ```ts
+     * function getCString(pointer: Deno.PointerObject, offset: number = 0) {
+     *   const view = new Deno.UnsafePointerView(pointer);
+     *   let byteLength = 0;
+     *   while (view.getUint8(offset + byteLength)) {
+     *     ++byteLength;
+     *   }
+     *   return new TextDecoder().decode(view.getArrayBuffer(byteLength, offset));
+     * }
+     * ``` */
     static getCString(pointer: PointerObject, offset?: number): string;
     /** Gets an `ArrayBuffer` of length `byteLength` at the specified byte
      * offset from the pointer. */
